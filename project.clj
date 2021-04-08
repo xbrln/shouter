28048bd4773c2e7b00462ab2d5ccc6a988726b12
xbrln <>
2021-04-08 11:47:51 +0200

    Add compojure and ring

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]]
   :repl-options {:init-ns shouter.core})
--- src/shouter/core.clj
+++ src/shouter/core.clj
@@ -4,3 +4,20 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(foo "g")
+
+(require '[clojure.java.jdbc :as sql])
+
+(sql/db-do-commands "postgresql://localhost:5432/shouter"
+                    (sql/create-table-ddl :testing [[:data :text]]))
+
+(sql/insert! "postgresql://localhost:5432/shouter"
+                    :testing {:data "Hello World 2"})
+
+
+(sql/query "postgresql://localhost:5432/shouter"
+                  ["select * from testing"])
+
+(sql/db-do-commands "postgresql://localhost:5432/shouter"
+                           "drop table testing")
