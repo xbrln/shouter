07a7ad952dc829673a26a977bacfe5a7d00f5d5c
xbrln <>
2021-04-09 08:21:56 +0200

    Add Procfile

--- project.clj
+++ project.clj
@@ -1,12 +1,21 @@
-(defproject shouter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+(defproject shouter "0.0.2"
+  :description "Shouter app"
+  :url "https://github.com/xbrln/shouter"
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.5"]]
-  :repl-options {:init-ns shouter.core})
+  :main ^:skip-aot shouter.web
+  :uberjar-name "shouter-standalone.jar"
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler shouter.web/application
+         :init shouter.models.migration/migrate}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}
+             :uberjar {:aot :all}})
